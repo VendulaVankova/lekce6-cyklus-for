c4ec3b2f469d7e423b9192b5252837b7bdec4b8e
Vendula Vankova <vendula.van@gmail.com>
2022-03-27 09:53:23 +0200

    Vyplnění zadání úkolu

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ console.log('První příklad - vzestupná řada 0 - 10');
 
 //TADY NAPIS CYKLUS
 
-console.log('-------------------');
-
+let vzestupnaRada = [];
+for (let i = 1; i <= 10; i = i + 1) {
+    console.log(i);
+}
 
 
 // Druhý příklad - zkopíruj předchozí příklad
@@ -19,8 +21,14 @@ console.log('Druhý příklad - rozdíl mezi <= a <');
 
 //TADY NAPIS CYKLUS
 
-console.log('-------------------');
 
+for (let i = 1; i <= 10; i = i + 1) {
+    console.log(i);
+}
+
+for (let i = 1; i < 10; i = i + 1) {
+    console.log(i);
+}
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
@@ -28,4 +36,63 @@ console.log('Třetí příklad - sestupná řada 10 - 0');
 
 //TADY NAPIS CYKLUS
 
-console.log('-------------------');
+
+for (let i = 10; i >= 0; i = i - 1) {
+    console.log(i);
+}
+
+
+// WHILE CYKLUS
+
+// První příklad - vypiš vzestupně čísla od 0 do 10
+console.log('První příklad s while cyklem - vzestupná řada 0 - 10');
+
+//TADY NAPIS CYKLUS
+
+let a = 0;
+
+while (a <= 10) {
+    console.log(a);
+    a++;
+}
+
+// Druhý příklad - zkopíruj předchozí příklad
+// a vyzkoušej, jaký je rozdíl mezi <= a <
+// v podmínce uvnitř cyklu
+
+console.log('Druhý příklad s while cyklem - rozdíl mezi <= a <');
+
+//TADY NAPIS CYKLUS
+
+console.log('<=');
+
+let b = 0;
+
+while (b <= 10) {
+    console.log(b);
+    b++;
+}
+
+console.log('<');
+
+let c = 0;
+
+while (c < 10) {
+    console.log(c);
+    c++;
+}
+
+
+
+
+// Třetí příklad s while cyklem - vypiš sestupně čísla od 10 do 0
+console.log('Třetí příklad s while cyklem - sestupná řada 10 - 0');
+
+//TADY NAPIS CYKLUS
+
+let d = 10;
+
+while (d >= 0) {
+   console.log(d);
+   d--;
+}
